7ab56f3dddce94c48471a1e780aa53dfc6508b2c
Narihan Ellaithy <narihanellaithy@gmail.com>
2018-05-04 00:19:58 +0300

    first

---
+++ src/main/java/Commands/Post/PostVideos.java
@@ -0,0 +1,44 @@
+package Commands.Post;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Envelope;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import Model.Videos;
+import Commands.Command;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+public class PostVideos extends Command{
+    public static int id = 0;
+    public static int subID = 0;
+    public void execute() {
+        HashMap<String, Object> props = parameters;
+        System.out.println(parameters);
+        Channel channel = (Channel) props.get("channel");
+        JSONParser parser = new JSONParser();
+
+        try {
+            JSONObject body = (JSONObject) parser.parse((String) props.get("body"));
+            JSONObject params = (JSONObject) parser.parse(body.get("body").toString());
+
+
+        AMQP.BasicProperties properties = (AMQP.BasicProperties) props.get("properties");
+        AMQP.BasicProperties replyProps = (AMQP.BasicProperties) props.get("replyProps");
+        Envelope envelope = (Envelope) props.get("envelope");
+        String response = Videos.postVideoByID(params);
+        try {
+            channel.basicPublish("", properties.getReplyTo(), replyProps, response.getBytes("UTF-8"));
+            channel.basicAck(envelope.getDeliveryTag(), false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/Model/Videos.java
@@ -0,0 +1,149 @@
+package Model;
+
+import com.arangodb.ArangoCursor;
+import com.arangodb.ArangoDB;
+import com.arangodb.ArangoDBException;
+import com.arangodb.entity.BaseDocument;
+import com.arangodb.entity.DocumentCreateEntity;
+import com.arangodb.util.MapBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import javax.swing.text.Document;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+public class Videos {
+
+    //Gets channels' info subscribed by user ID(user ID = _key and maps to array of channels' IDs)
+    public static String getVideoByID(int id) {
+        ArangoDB arangoDB = new ArangoDB.Builder().build();
+        String dbName = "subscriptions";
+        String collectionName = "Videos";
+        JSONObject videoObjectM = new JSONObject();
+        System.out.println("hena");
+        try {
+            BaseDocument myDocument = arangoDB.db(dbName).collection(collectionName).getDocument("" + id,
+                    BaseDocument.class);
+
+            JSONObject subscriptionObject = new JSONObject();
+            videoObjectM.put("VideoID",id);
+            videoObjectM.put("ChannelID",myDocument.getAttribute("channel_id"));
+            videoObjectM.put("Likes",myDocument.getAttribute("likes"));
+            videoObjectM.put("Dislikes",myDocument.getAttribute("dislikes"));
+            videoObjectM.put("Views",myDocument.getAttribute("views"));
+            videoObjectM.put("Title",myDocument.getAttribute("title"));
+            videoObjectM.put("Category",myDocument.getAttribute("category"));
+            videoObjectM.put("Duration",myDocument.getAttribute("duration"));
+            videoObjectM.put("Description",myDocument.getAttribute("description"));
+            videoObjectM.put("Qualities",myDocument.getAttribute("qualities"));
+            videoObjectM.put("Private",myDocument.getAttribute("private"));
+            videoObjectM.put("Date_Created",myDocument.getAttribute("date_created"));
+            videoObjectM.put("Date_Modified",myDocument.getAttribute("date_modified"));
+
+         } catch (ArangoDBException e) {
+            System.err.println("Failed to get document: myKey; " + e.getMessage());
+        }
+        System.out.println(videoObjectM.toString());
+        return videoObjectM.toString();
+
+
+    }
+
+    public static String getVideoChannelsByID(int channel_id) {
+        ArangoDB arangoDB = new ArangoDB.Builder().build();
+        String dbName = "subscriptions";
+        String collectionName = "videos";
+        JSONObject videoObject = new JSONObject();
+        String subs = "";
+        JSONArray videoArray = new JSONArray();
+        String query = "FOR doc IN Videos\n" +
+                 "        FILTER doc.`channel_id` like @value\n" +
+                "        RETURN doc";
+        Map<String, Object> bindVars = new MapBuilder().put("value", channel_id).get();
+
+        ArangoCursor<BaseDocument> cursor = arangoDB.db(dbName).query(query, bindVars, null,
+                BaseDocument.class);
+        if(cursor.hasNext()) {
+            BaseDocument cursor2=null;
+            int id2=0;
+            for (; cursor.hasNext(); ) {
+                cursor2 = cursor.next();
+                System.out.println(cursor2.getKey());
+                JSONObject videoObjectM = new JSONObject();
+                BaseDocument myDocument2 = arangoDB.db(dbName).collection("Videos").getDocument(cursor2.getKey(),
+                        BaseDocument.class);
+                int video_id= Integer.parseInt(cursor2.getKey());
+
+                videoObjectM.put("VideoID", video_id);
+                videoObjectM.put("ChannelID", channel_id);
+                videoObjectM.put("Likes", Integer.parseInt(""+myDocument2.getAttribute("likes")));
+                videoObjectM.put("Dislikes", Integer.parseInt(""+myDocument2.getAttribute("dislikes")));
+                videoObjectM.put("Views", Integer.parseInt(""+myDocument2.getAttribute("views")));
+                videoObjectM.put("Title", myDocument2.getAttribute("title"));
+                videoObjectM.put("Category", myDocument2.getAttribute("category"));
+                videoObjectM.put("Duration", Integer.parseInt(""+myDocument2.getAttribute("duration")));
+                videoObjectM.put("Description", myDocument2.getAttribute("description"));
+                videoObjectM.put("Qualities", myDocument2.getAttribute("qualities"));
+                videoObjectM.put("Private", myDocument2.getAttribute("private"));
+                videoObjectM.put("Date_Created", myDocument2.getAttribute("date_created"));
+                videoObjectM.put("Date_Modified", myDocument2.getAttribute("date_modified"));
+                videoArray.add(videoObjectM);
+            }
+            videoObject.put("Channel  "+channel_id+" Videos:",videoArray);
+        }
+        else{
+            videoObject.put("No videos found for channel  ",channel_id);
+        }
+
+        System.out.println(videoObject.toString());
+        return videoObject.toString();
+
+
+    }
+
+
+    public static String postVideoByID(JSONObject params){
+        ArangoDB arangoDB = new ArangoDB.Builder().build();
+        String dbName = "subscriptions";
+        String collectionName = "Videos";
+        BaseDocument myObject = new BaseDocument();
+        JSONObject videoObject = new JSONObject();
+        myObject.addAttribute("channel_id", Integer.parseInt(params.get("channel_id").toString()));
+        myObject.addAttribute("likes", 0);//Integer.parseInt(params.get("likes").toString()));
+        myObject.addAttribute("dislikes", 0);//Integer.parseInt(params.get("dislikes").toString()));
+        myObject.addAttribute("views", 0);//Integer.parseInt(params.get("views").toString()));
+        myObject.addAttribute("title", params.get("title").toString());
+        myObject.addAttribute("category", params.get("category").toString());
+        myObject.addAttribute("duration", Integer.parseInt(params.get("duration").toString()));
+        myObject.addAttribute("description", params.get("description").toString());
+        myObject.addAttribute("qualities", params.get("qualities"));
+        myObject.addAttribute("private", params.get("private"));
+        myObject.addAttribute("date_created", new Timestamp(System.currentTimeMillis()));
+        myObject.addAttribute("date_modified", new Timestamp(System.currentTimeMillis()));
+
+        try{
+            arangoDB.db(dbName).collection(collectionName).insertDocument(myObject);
+            System.out.println("Document created");
+
+        } catch (ArangoDBException e) {
+            System.err.println("Failed to create document. " + e.getMessage());
+        }
+        return videoObject.toString();
+
+    }
+//
+    public static String deleteVideoByID(int id){
+        ArangoDB arangoDB = new ArangoDB.Builder().build();
+        String dbName = "subscriptions";
+        String collectionName = "Video";
+
+        ArrayList<Long> ids = new ArrayList<>();
+
+        arangoDB.db(dbName).collection(collectionName).deleteDocument("" + id);
+
+        return true+"";
+    }
+}
